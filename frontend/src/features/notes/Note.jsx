18a2336a3552aa6b27061d30459cbfd749d51813
process70 <163150253+process70@users.noreply.github.com>
2024-11-17 17:54:33 +0000

    Update Note.jsx

--- frontend/src/features/notes/Note.jsx
+++ frontend/src/features/notes/Note.jsx
@@ -29,7 +29,7 @@ const Note = ({noteId}) => {
                 <td className="table__cell note__created">{created}</td>
                 <td className="table__cell note__updated">{updated}</td>
                 <td className="table__cell note__title">{note.title}</td>
-                <td className="table__cell note__username">{note.username}</td>
+                <td className="table__cell note__username">{note.user.username}</td>
                 <td className="table__cell">
                     <button className="icon-button table__button" onClick={handleEdit}>                    
                         <FontAwesomeIcon icon={faPenToSquare} />
