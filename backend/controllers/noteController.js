9d3780f7ee95d82e200f0ce6fb667d2689fb272a
process70 <163150253+process70@users.noreply.github.com>
2024-11-17 16:27:27 +0000

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -10,7 +10,7 @@ const getAllNotes = asyncHandler(async (req, res) => {
     
     // waits for all the asynchronous operations to complete before assigning the result to notesWithUser
     // sending the response with the actual usernames added to the data
-    const notesWithUser = notes.map((note) => {
+    const notesWithUser = notes.map(async(note) => {
         const user = await User.findById(note.user)
         note = { ...note, username: user.username}
         //returning the edited note
